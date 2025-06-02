d9918f3efdc2dc803bc610b9baf30e75f235c05d
Filp <canefil@proton.me>
2025-06-02 19:32:07 +0200

    hero title change

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,10 @@
 export default function Hero() {
     return (
         <div className="flex flex-col items-center justify-center mt-30 text-center">
-            <h1 className="font-extrabold text-5xl pb-8 text-zinc-700 ">Tvoříme budoucnost aplikací.</h1>
-            <p className="font-medium text-xl text-center pb-8 text-zinc-700">Vytváříme inovativní aplikace, které mění způsob, <br />jakým lidé žijí a pracují.</p>
+            <h1 className="font-extrabold text-5xl pb-8 text-zinc-700 ">We are shaping the future of applications.</h1>
+            <p className="font-medium text-xl text-center pb-8 text-zinc-700">We create innovative applications that change the way <br />people live and work.</p>
             <button className="text-lg bg-lime-400 text-zinc-700 font-semibold px-5 py-2 rounded-2xl hover:bg-lime-500 transition duration-300">
-               Portfolio bude brzy k dispozici
+               Portfolio coming soon
             </button>
         </div>
 
